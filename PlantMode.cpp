6d5395029bf59da09bd07cdb3bee20221a07e187
Sartharis <jan.orlowsk@gmail.com>
2019-10-21 20:59:06 -0400

    Added tiles

--- PlantMode.cpp
+++ PlantMode.cpp
@@ -8,6 +8,7 @@
 #include "gl_errors.hpp"
 #include "data_path.hpp"
 #include "load_save_png.hpp"
+#include "collide.hpp"
 
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -19,11 +20,16 @@
 #include <random>
 #include <unordered_map>
 
-Mesh const *plant_tile = nullptr;
+GroundTileType const* sea_tile = nullptr;
+GroundTileType const* ground_tile = nullptr;
+
+Mesh const* sea_tile_mesh = nullptr;
+Mesh const* ground_tile_mesh = nullptr;
 
 Load< MeshBuffer > plant_meshes(LoadTagDefault, [](){
-	auto ret = new MeshBuffer(data_path("plant.pnct"));
-	plant_tile = &ret->lookup("Tile");
+	auto ret = new MeshBuffer(data_path("solidarity.pnct"));
+	sea_tile_mesh = &ret->lookup("SeaTile");
+	ground_tile_mesh = &ret->lookup("GroundTile");
 	return ret;
 });
 
@@ -31,82 +37,91 @@ Load< GLuint > plant_meshes_for_lit_color_texture_program(LoadTagDefault, [](){
 	return new GLuint(plant_meshes->make_vao_for_program(lit_color_texture_program->program));
 });
 
-BoneAnimation::Animation const *plant_banim_wind = nullptr;
-BoneAnimation::Animation const *plant_banim_walk = nullptr;
+GroundTileType::GroundTileType( bool can_plant_in, const Mesh* tile_mesh_in )
+	: can_plant(can_plant_in), mesh(tile_mesh_in)
+{
+}
 
-Load< BoneAnimation > plant_banims(LoadTagDefault, [](){
-	auto ret = new BoneAnimation(data_path("plant.banims"));
-	plant_banim_wind = &(ret->lookup("Wind"));
-	plant_banim_walk = &(ret->lookup("Walk"));
-	return ret;
-});
+const Mesh* GroundTileType::get_mesh() const
+{
+	return mesh;
+}
 
-Load< GLuint > plant_banims_for_bone_lit_color_texture_program(LoadTagDefault, [](){
-	return new GLuint(plant_banims->make_vao_for_program(bone_lit_color_texture_program->program));
-});
+void GroundTile::change_tile_type( const GroundTileType* tile_type_in )
+{
+	tile_type = tile_type_in;
+	drawable->pipeline.start = tile_type->get_mesh()->start;
+	drawable->pipeline.count = tile_type->get_mesh()->count;
+}
 
-PlantMode::PlantMode() {
-	//Make a scene from scratch using the plant prop and the tile mesh:
-	{ //make a tile floor:
+
+PlantMode::PlantMode() 
+{
+	{
+		sea_tile = new GroundTileType( false, sea_tile_mesh );
+		ground_tile = new GroundTileType( true, ground_tile_mesh );
+	}
+
+	// Make the tile grid
+	{
+		grid = new GroundTile*[plant_grid_x];
+		for( int32_t x = 0; x < plant_grid_x; ++x ) 
+		{
+			grid[x] = new GroundTile[plant_grid_y];
+		}
+	}
+
+	//Populate the tile grid (default is sea)
+	{
 		Scene::Drawable::Pipeline tile_info;
 		tile_info = lit_color_texture_program_pipeline;
 		tile_info.vao = *plant_meshes_for_lit_color_texture_program;
-		tile_info.start = plant_tile->start;
-		tile_info.count = plant_tile->count;
+		tile_info.start = 0;
+		tile_info.count = 0;
 
-		for (int32_t x = -5; x <= 5; ++x) {
-			for (int32_t y = -5; y <= 5; ++y) {
+		glm::vec3 tile_center_pos = glm::vec3( ( (float)plant_grid_x - 1 ) * plant_grid_tile_size.x / 2.0f, ( (float)plant_grid_y - 1 ) * plant_grid_tile_size.y / 2.0f, 0.0f );
+
+		for( int32_t x = 0; x < plant_grid_x; ++x ) 
+		{
+			for( int32_t y = 0; y < plant_grid_y; ++y ) 
+			{
+				// Set coordinates
+				grid[x][y].grid_x = x;
+				grid[x][y].grid_y = y;
+
+				// Set up drawable and initial pipline for each tile
 				scene.transforms.emplace_back();
-				Scene::Transform *transform = &scene.transforms.back();
-				transform->name = "Tile-" + std::to_string(x) + "," + std::to_string(y); //<-- no reason to do this, we don't have scene debugger or anything
-				transform->position = glm::vec3(2.0f*x, 2.0f*y, 0.0f);
-
-				scene.drawables.emplace_back(transform);
-				Scene::Drawable *tile = &scene.drawables.back();
+				Scene::Transform* transform = &scene.transforms.back();
+				transform->position = glm::vec3( plant_grid_tile_size.x * x, plant_grid_tile_size.y * y, 0.0f ) - tile_center_pos;
+				scene.drawables.emplace_back( transform );
+				Scene::Drawable* tile = &scene.drawables.back();
 				tile->pipeline = tile_info;
+				grid[x][y].drawable = tile;
+
+				// Set default type for the tile
+				grid[x][y].change_tile_type(sea_tile);
+
 			}
 		}
 	}
 
-	{ //put some plants around the edge:
-		Scene::Drawable::Pipeline plant_info;
-		plant_info = bone_lit_color_texture_program_pipeline;
 
-		plant_info.vao = *plant_banims_for_bone_lit_color_texture_program;
-		plant_info.start = plant_banims->mesh.start;
-		plant_info.count = plant_banims->mesh.count;
-
-		plant_animations.reserve(5);
-		for (int32_t x = -2; x <= 2; ++x) {
-			if (x != 0) {
-				plant_animations.emplace_back(*plant_banims, *plant_banim_wind, BoneAnimationPlayer::Once);
-				plant_animations.back().position = 1.0f;
-			} else {
-				plant_animations.emplace_back(*plant_banims, *plant_banim_walk, BoneAnimationPlayer::Loop, 0.0f);
+	// Create a lil center island
+	{
+		for( int32_t x = 7; x < 13; ++x )
+		{
+			for( int32_t y = 8; y < 12; ++y )
+			{
+				//grid[x][y].change_tile_type( ground_tile );
 			}
-
-			BoneAnimationPlayer *player = &plant_animations.back();
-		
-			plant_info.set_uniforms = [player](){
-				player->set_uniform(bone_lit_color_texture_program->BONES_mat4x3_array);
-			};
-
-			scene.transforms.emplace_back();
-			Scene::Transform *transform = &scene.transforms.back();
-			transform->position.x = x * 2.5f;
-			scene.drawables.emplace_back(transform);
-			Scene::Drawable *plant = &scene.drawables.back();
-			plant->pipeline = plant_info;
-
-			if (x == 0) this->plant = plant;
-		};
-		assert(plant_animations.size() == 5);
+		}
 	}
+
 
 	{ //make a camera:
 		scene.transforms.emplace_back();
 		Scene::Transform *transform = &scene.transforms.back();
-		transform->position = glm::vec3(0.0f, -10.0f, 3.0f);
+		transform->position = glm::vec3(0.0f, 0.0f, 0.0f);
 		transform->rotation = glm::quat_cast(glm::mat3(glm::lookAt(
 			transform->position,
 			glm::vec3(0.0f, 0.0f, 0.0f),
@@ -129,86 +144,85 @@ bool PlantMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size
 		return false;
 	}
 
-	if (evt.type == SDL_KEYDOWN && evt.key.keysym.scancode == SDL_SCANCODE_UP) {
-		forward = true;
-		return true;
-	}
-	if (evt.type == SDL_KEYUP && evt.key.keysym.scancode == SDL_SCANCODE_UP) {
-		forward = false;
-		return true;
-	}
-	if (evt.type == SDL_KEYDOWN && evt.key.keysym.scancode == SDL_SCANCODE_DOWN) {
-		backward = true;
-		return true;
-	}
-		if (evt.type == SDL_KEYUP && evt.key.keysym.scancode == SDL_SCANCODE_DOWN) {
-		backward = false;
-		return true;
-	}
-
-	if (evt.type == SDL_MOUSEBUTTONDOWN) {
-		if (!mouse_captured) {
-			SDL_SetRelativeMouseMode(SDL_TRUE);
-			mouse_captured = true;
-		}
-	}
-	if (evt.type == SDL_MOUSEBUTTONUP) {
-		if (mouse_captured) {
-			SDL_SetRelativeMouseMode(SDL_FALSE);
-			mouse_captured = false;
-		}
-	}
-	if (evt.type == SDL_MOUSEMOTION) {
-		if (mouse_captured) {
-			float yaw = evt.motion.xrel / float(window_size.y) * camera->fovy;
-			float pitch = -evt.motion.yrel / float(window_size.y) * camera->fovy;
-
-			//update camera angles:
-			camera_elevation = glm::clamp(camera_elevation + pitch, glm::radians(10.0f), glm::radians(80.0f));
-			camera_azimuth = camera_azimuth + yaw;
-		}
-	}
-
 	return false;
 }
 
-void PlantMode::update(float elapsed) {
+void PlantMode::update(float elapsed) 
+{
+	camera_azimuth += elapsed;
 
+	// Update Camera Position
 	{
-		float step = 0.0f;
-		if (forward) step += elapsed * 4.0f;
-		if (backward) step -= elapsed * 4.0f;
-		plant->transform->position.y += step;
-		plant_animations[2].position += step / 1.88803f;
-		plant_animations[2].position -= std::floor(plant_animations[2].position);
+		float ce = std::cos( camera_elevation );
+		float se = std::sin( camera_elevation );
+		float ca = std::cos( camera_azimuth );
+		float sa = std::sin( camera_azimuth );
+		camera->transform->position = camera_radius * glm::vec3( ce * ca, ce * sa, se );
+		camera->transform->rotation =
+			glm::quat_cast( glm::transpose( glm::mat3( glm::lookAt(
+			camera->transform->position,
+			glm::vec3( 0.0f, 0.0f, 0.0f ),
+			glm::vec3( 0.0f, 0.0f, 1.0f )
+			) ) ) );
 	}
 
-	float ce = std::cos(camera_elevation);
-	float se = std::sin(camera_elevation);
-	float ca = std::cos(camera_azimuth);
-	float sa = std::sin(camera_azimuth);
-	camera->transform->position = camera_radius * glm::vec3(ce * ca, ce * sa, se) + plant->transform->position;
-	camera->transform->rotation =
-		glm::quat_cast(glm::transpose(glm::mat3(glm::lookAt(
-			camera->transform->position,
-			plant->transform->position,
-			glm::vec3(0.0f, 0.0f, 1.0f)
-		))));
-	
-	static std::mt19937 mt(0xfeedf00d);
-	wind_acc -= elapsed;
-	if (wind_acc < 0.0f) {
-		wind_acc = mt() / float(mt.max()) * 2.0f;
-		uint32_t idx = mt() % plant_animations.size();
-		if (idx != 2) {
-			if (plant_animations[idx].done()) {
-				plant_animations[idx].position = 0.0f;
+	// Query for clicked tile
+	const Uint32 state = SDL_GetMouseState( NULL, NULL );
+	if( state & SDL_BUTTON( SDL_BUTTON_LEFT ) )
+	{
+		float col_check_dist = 1000.0f;
+		glm::vec3 from_camera_start = camera->transform->position;
+		glm::vec3 from_camera_dir =  camera->transform->rotation * glm::vec3( 0.0f, 0.0f, -1.0f );
+
+		// Check collision against each tile
+		for( int32_t x = 0; x < plant_grid_x; ++x )
+		{
+			for( int32_t y = 0; y < plant_grid_y; ++y )
+			{
+				// For now do a small sphere sweep against each triangle (TODO: optimize to line vs box collision if this is really bad)
+				float sphere_radius = 0.0001f; 
+				glm::vec3 sphere_sweep_from = from_camera_start;
+				glm::vec3 sphere_sweep_to = from_camera_start + col_check_dist * from_camera_dir;
+
+				glm::vec3 sphere_sweep_min = glm::min( sphere_sweep_from, sphere_sweep_to ) - glm::vec3( sphere_radius );
+				glm::vec3 sphere_sweep_max = glm::max( sphere_sweep_from, sphere_sweep_to ) + glm::vec3( sphere_radius );
+
+				GroundTile* collided_tile = nullptr;
+				float collision_t = 1.0f;
+				glm::vec3 collision_at = glm::vec3( 0.0f );
+				glm::vec3 collision_out = glm::vec3( 0.0f );
+
+
+				glm::mat4x3 collider_to_world = grid[x][y].drawable->transform->make_local_to_world();
+				const Mesh& collider_mesh = *(grid[x][y].tile_type->get_mesh());
+
+				assert( collider_mesh.type == GL_TRIANGLES ); //only have code for TRIANGLES not other primitive types
+				for( GLuint v = 0; v + 2 < collider_mesh.count; v += 3 ) 
+				{
+					
+					//get vertex positions from associated positions buffer:
+					glm::vec3 a = collider_to_world * glm::vec4( plant_meshes->positions[collider_mesh.start + v + 0], 1.0f );
+					glm::vec3 b = collider_to_world * glm::vec4( plant_meshes->positions[collider_mesh.start + v + 1], 1.0f );
+					glm::vec3 c = collider_to_world * glm::vec4( plant_meshes->positions[collider_mesh.start + v + 2], 1.0f );
+					//check triangle:
+					bool did_collide = collide_swept_sphere_vs_triangle(
+						sphere_sweep_from, sphere_sweep_to, sphere_radius,
+						a, b, c,
+						&collision_t, &collision_at, &collision_out );
+
+					if( did_collide ) 
+					{
+						printf( "a" );
+						collided_tile = &grid[x][y];
+					}
+				}
+
+				if( collided_tile )
+				{
+					collided_tile->change_tile_type( ground_tile );
+				}
 			}
 		}
-	}
-
-	for (auto &anim : plant_animations) {
-		anim.update(elapsed);
 	}
 }
 
@@ -231,3 +245,4 @@ void PlantMode::draw(glm::uvec2 const &drawable_size) {
 
 	GL_ERRORS();
 }
+
--- PlantMode.hpp
+++ PlantMode.hpp
@@ -12,30 +12,52 @@
 #include <vector>
 #include <list>
 
-// The 'PlantMode' mode shows off some bone animations:
+struct Plant
+{
+	virtual void update( float elapsed );
+};
 
+struct GroundTileType
+{
+	GroundTileType( bool can_plant_in, const Mesh* tile_mesh_in );
+	const Mesh* get_mesh() const;
+
+private:
+	bool can_plant = true;
+	const Mesh* mesh = nullptr;
+};
+
+struct GroundTile
+{
+	void change_tile_type( const GroundTileType* tile_type_in );
+
+	const GroundTileType* tile_type = nullptr;
+	Plant* active_plant = nullptr;
+	Scene::Drawable* drawable = nullptr;
+	int grid_x = 0;
+	int grid_y = 0;
+};
+
+// The 'PlantMode':
 struct PlantMode : public Mode {
 	PlantMode();
 	virtual ~PlantMode();
+
 
 	virtual bool handle_event(SDL_Event const &evt, glm::uvec2 const &window_size) override;
 	virtual void update(float elapsed) override;
 	virtual void draw(glm::uvec2 const &drawable_size) override;
 
-	//controls:
-	bool mouse_captured = false;
-	bool forward = false;
-	bool backward = false;
-
 	//scene:
 	Scene scene;
-	Scene::Drawable *plant = nullptr;
 	Scene::Camera *camera = nullptr;
-	float camera_radius = 10.0f;
-	float camera_azimuth = glm::radians(60.0f);
+
+	GroundTile** grid = nullptr;
+	glm::vec2 plant_grid_tile_size = glm::vec2( 2.0f, 2.0f );
+	int plant_grid_x = 20;
+	int plant_grid_y = 20;
+
+	float camera_radius = 15.0f;
+	float camera_azimuth = glm::radians(90.0f);
 	float camera_elevation = glm::radians(45.0f);
-
-	std::vector< BoneAnimationPlayer > plant_animations;
-
-	float wind_acc = 0.0f;
 };
